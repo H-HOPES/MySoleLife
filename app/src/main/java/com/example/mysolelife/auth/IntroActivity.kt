b5aead07f6a37fd311ba7260a2a12514c44f9f29
H-HOPES <htjdgns@naver.com>
2025-07-04 16:19:23 +0900

    Login Function realization

---
+++ app/src/main/java/com/example/mysolelife/auth/IntroActivity.kt
@@ -0,0 +1,36 @@
+package com.example.mysolelife.auth
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.mysolelife.R
+import com.example.mysolelife.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+
+        binding.loginBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.joinBtn.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
